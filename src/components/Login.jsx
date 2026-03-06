67a19957a0ea896caf88aeb90b74b3a835e437be
YEISSON <yeisonossa3010@gmail.com>
2026-03-05 21:55:28 -0500

    Implementacion de landingPage

---
+++ src/components/Login.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import logo from "../assets/logo.png";
+import Foto from "../assets/left-photo.jpg";
+
+
+export default function Login() {
+    return (
+        <div className="min-h-screen flex">
+            {/* Izquierda: imagen + texto (oculta en móviles) */}
+            <div className="hidden md:flex w-1/2 bg-cover bg-center relative" style={{ backgroundImage: `url(${Foto})` }}>
+                <div className="bg-black/10 w-full h-full flex items-end p-8">
+                    <div className="text-white max-w-md">
+                        <h1 className="text-4xl font-semibold">Impulsando a la próxima generación.</h1>
+                        <p className="mt-4 text-2xl">Accede a tus registros académicos, horarios y recursos de aprendizaje en un espacio de trabajo moderno.</p>
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+
+    )
+
+}
